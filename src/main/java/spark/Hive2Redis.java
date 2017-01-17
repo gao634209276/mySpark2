d4fe28171c8bb14961cfd932927af510fb1e21ad
hadoop <634209276@qq.com>
2017-01-17 20:33:30 +0800

    redis

--- src/main/java/spark/Hive2Redis.java
+++ src/main/java/spark/Hive2Redis.java
@@ -6,10 +6,15 @@ import org.apache.spark.api.java.function.ForeachPartitionFunction;
 import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.VoidFunction;
 import org.apache.spark.sql.*;
+import redis.RedisClient;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Pipeline;
 import scala.Function1;
 import scala.Tuple2;
 import scala.collection.Iterator;
 import scala.runtime.BoxedUnit;
+
+import java.io.Serializable;
 
 /**
  */
@@ -17,7 +22,7 @@ public class Hive2Redis {
 
 	public static void main(String[] args) {
 		//Logger.getLogger("org.apache.spark").setLevel(Level.WARN);
-		String warehouseLocation = "hdfs:///user/sinova/hive/warehouse";
+		String warehouseLocation = "hdfs:///user/hive/warehouse";
 		SparkSession spark = SparkSession.builder()
 				.appName("Hive2Redis")
 				.enableHiveSupport()
@@ -25,47 +30,26 @@ public class Hive2Redis {
 				.getOrCreate();
 
 		// 从java bean获取到一个Encoder
-		Encoder<Words> personEncoder = Encoders.bean(Words.class);
-		Dataset<Words> ds = spark.table("test.words").as(personEncoder);
+		//Encoder<Words> personEncoder = Encoders.bean(Words.class);
+		Dataset<Row> ds = spark.table("test.words");
 		//df.show();
 		//df.select(col("word"), col("freq").plus(1)).show();
-		ds.foreach(new ForeachFunction<Words>() {
-			public void call(Words words) throws Exception {
-				System.out.println(words.toString());
+		ds.foreachPartition(new ForeachPartitionFunction<Row>() {
+			@Override
+			public void call(java.util.Iterator<Row> t) throws Exception {
+
+				Jedis jedis = RedisClient.getRedisClient().getJedis();
+				Pipeline pl = jedis.pipelined();
+				Row r;
+				while (t.hasNext()) {
+					r = t.next();
+					jedis.select(1);
+					jedis.zadd("test", r.getInt(0), r.getString(1));
+				}
+				pl.sync();
+				RedisClient.getRedisClient().returnJedis(jedis);
 			}
 		});
 	}
 
-	/**
-	 * java bean
-	 */
-	private static class Words {
-
-		private int freq;
-		private String word;
-
-		public String getWord() {
-			return word;
-		}
-
-		public void setWord(String word) {
-			this.word = word;
-		}
-
-		public int getFreq() {
-			return freq;
-		}
-
-		public void setFreq(int freq) {
-			this.freq = freq;
-		}
-
-		@Override
-		public String toString() {
-			return "Words{" +
-					"freq=" + freq +
-					", word='" + word + '\'' +
-					'}';
-		}
-	}
 }
